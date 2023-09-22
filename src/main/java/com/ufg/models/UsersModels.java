8e82a34ef5b9b5ea2721623ff20ce4d08963682f
Hector Alvarado <hectoralexanderalvarado@gmail.com>
2023-09-21 19:26:08 -0600

    Se creo el modelo para los clientes con sus respectivos getters y setters

--- src/main/java/com/ufg/models/UsersModels.java
+++ src/main/java/com/ufg/models/UsersModels.java
@@ -9,5 +9,67 @@ package com.ufg.models;
  * @author hecto
  */
 public class UsersModels {
-    
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private Double saldo;
+    private Boolean estado;
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
 }
